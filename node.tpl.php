d69fbfc4e2a4f6105712596682468ac93bfb38cc
onsch <ons@com-unique.tn>
2017-10-16 11:34:59 +0100

    espace

--- node.tpl.php
+++ node.tpl.php
@@ -75,6 +75,8 @@
  */
 ?>
 <?php if (!$page): ?>
+
+  
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <?php endif; ?>
   <?php if (!$page): ?>
